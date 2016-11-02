b3b433ab5a2a9a7b817f633f67defee54ba9567a
Jack Mocherman <mochermanjack@gmail.com>
2016-11-02 10:39:31 -0500

    pushing up portfolio page

---
+++ scripts/main.js
@@ -0,0 +1,16 @@
+'use strict';
+$(document).ready(function(){
+  $('.parallax').parallax();
+  $(".button-collapse").sideNav();
+  $('.scrollspy').scrollSpy();
+});
+
+$(function() {
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 2000, 'easeInOutSine');
+        event.preventDefault();
+    });
+});
